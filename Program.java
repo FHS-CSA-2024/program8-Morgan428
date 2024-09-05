d1dbe45cd9e0a034f41976e5d2f85c0e39cfb6c4
Morgan428 <MorganWare428@gmail.com>
2024-09-05 12:33:29 -0700

    Update Program.java
    Done!

--- Program.java
+++ Program.java
@@ -1,58 +1,41 @@
-package main.java;
 import java.util.Scanner;
-//import stuff here
-
-//Your code here
-
-public class Program7{
-    //Define your final variables here
-    // **HINT: Maybe the conversion rates?????**
+public class program8{
     public static void main(String[] args){
-        //Make you own test code here
-        Program7 tester = new Program7();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter first int: ");
+        final int inA = scan.nextInt();
+        System.out.println("Enter last int: ");
+        final int inB = scan.nextInt();
+        final int sum = inA + inB;
+        final int dif = inA - inB;
+        final int pro = inA * inB;
+        final double ave = (inA + inB) / 2.0;
+        final int dis = Math.abs(dif);
+        final int max = (-(inA - inB) + inA + inB) / 2;
+        final int min = ((inA - inB) + inA + inB) / 2;
         
-        double myTestVal = 0.0;
-        
-        myTestVal = tester.convertMoney(0,0,0);
-        
-        System.out.println(myTestVal);
+        System.out.println("Original numbers are " + inA + " and " + inB);
+        System.out.println("Sum = " + sum);
+        System.out.println("Difference = " + dif);
+        System.out.println("Product = " + pro);
+        System.out.println("Average = " + ave);
+        System.out.println("Distance = "  + dis);
+        System.out.println("Max = " + max);
+        System.out.println("Min = " + min);
     }
-    
-    public double convertMoney(int schruteBucks, int stanleyNickels, int klevins){
-        // The user will input the following three
-        Scanner s = new Scanner(System.in);
-        //
-        System.out.println("Enter Schrute-bucks");
-        int sBuck = s.nextInt();
-        //
-        System.out.println("Enter Klevins");
-        int kle = s.nextInt();
-        //
-        System.out.println("Enter Stanley-nickels");
-        int sNic = s.nextInt();
-        //
-        double sNicTemp = 0; // temporaly holding the value of new Stanley-nickels before they get turned into a int int insted of a double
-        final double sNicCon = 2.4; // for converting old Stanley-nickels to new Stanley-nickels
-        if (kle == 0){
-            sNicTemp = sNic * sNicCon;
-            sNic = (int)sNicTemp; // remove "."'s
-        }
-        else {
-            int sNicOLd = kle * 12;
-            sNicTemp = (sNicOLd + sNic) / sNicCon;
-            sNicTemp = Math.round(sNicTemp);
-            sNic = (int)sNicTemp; // remove "."'s
-        }
-        if (sNic >= 100){
-            sBuck = sNic / 100 + sBuck;
-            sNic = sNic % 100;
-        }
-        else {
-        sNic = sNic / 10;
-        }
-        System.out.println(sBuck + "." + sNic);
-        return 0;
-    }
-    
-    
 }
+//Paste console output below:
+/*
+Enter first int: 
+13
+Enter last int: 
+20
+Original numbers are 13 and 20
+Sum = 33
+Difference = -7
+Product = 260
+Average = 16.5
+Distance = 7
+Max = 20
+Min = 13
+*/
